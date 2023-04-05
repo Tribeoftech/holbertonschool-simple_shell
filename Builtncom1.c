fb2281be5910f6e942ed39b99c57f7c3ea8dd828
Tribeoftech <gratefulgrowing333@gmail.com>
2023-04-05 10:06:32 -0700

    shell

---
+++ Builtncom1.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+ * sh_alias - function that creates alias
+ * @argv: The input string
+ * Return: 0  on success.
+ */
+int sh_alias(char **argv)
+{
+	argv = argv;
+	return (0);
+:x
